b19ed9d6137734cced443b10b9cfe61cf2d29988
psrwt <praveensingh.connect@gmail.com>
2025-08-14 11:02:35 +0530

    everything good

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-black text-6xl font-bold">Hi, I@apos;m Praveen Singh Rawat</h1>
+      <h1 className="text-black text-6xl font-bold">Hi, I&apos;m Praveen Singh Rawat</h1>
     </main>
   );
 }
