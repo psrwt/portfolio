20e499adf851d7ee2f2bd8c6215f2fae0911eeae
psrwt <praveensingh.connect@gmail.com>
2025-08-14 10:55:17 +0530

    psrawat.github.io initialized

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // This is the crucial line that enables static exports
   output: 'export',
-
-  // The two lines below are for deploying to a subdirectory on GitHub Pages
-  // The name of your GitHub repository is 'portfolio'
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  
 };
 
 export default nextConfig;
