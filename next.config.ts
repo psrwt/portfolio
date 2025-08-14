f86ceda8b9a533c877f14364953fba9fcb47f6c3
psrwt <praveensingh.connect@gmail.com>
2025-08-14 10:37:48 +0530

    configure github pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // This is the crucial line that enables static exports
+  output: 'export',
+
+  // The two lines below are for deploying to a subdirectory on GitHub Pages
+  // The name of your GitHub repository is 'portfolio'
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
