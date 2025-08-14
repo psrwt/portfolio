2cfe68f277ce3e5eb8855bbc7e4738e5e11c3cfb
psrwt <praveensingh.connect@gmail.com>
2025-08-14 10:43:48 +0530

    page.tsx change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
     <main className="min-h-screen bg-white flex items-center justify-center">
-      <h1 className="text-black text-6xl fond-bold">Hi, I'm Praveen Singh Rawat</h1>
+      {/* 
+        The main container uses flexbox to center the h1 element.
+        'min-h-screen' ensures it covers the full viewport height.
+        The background is set to white.
+      */}
+      <h1 className="text-black text-6xl font-bold">
+        hi
+      </h1>
     </main>
   );
 }
